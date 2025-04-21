6d5ed3139acb852853dfa2a20f2adcf736966aae
Nick Austin <nickaustin27@icloud.com>
2025-04-20 17:30:27 -0700

    Added getRandomString()

--- p2-random.js
+++ p2-random.js
@@ -5,22 +5,32 @@
 
 // Returns a random number between min (inclusive) and max (exclusive)
 function getRandomInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  }
-  
-  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-  let result = "";
-  
-  for (let i = 0; i < getRandomInteger(5, 26); i++) {
-    result += alphabet[getRandomInteger(1, alphabet.length - 1)];
-  }
-  
-  console.log(result);
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+let result = "";
+
+for (let i = 0; i < getRandomInteger(5, 26); i++) {
+  result += alphabet[getRandomInteger(1, alphabet.length - 1)];
+}
+
+console.log(getRandomString());
 
 // Returns a single random lowercase letter
-function getRandomLetter(){
+function getRandomLetter() {
   return alphabet[Math.floor(Math.random() * alphabet.length)];
 }
 
 console.log(getRandomLetter());
+
+// Returns the random length string
+function getRandomString(minLength = 10, maxLength = 20) {
+  const length =
+    Math.floor(Math.random() * (maxLength - minLength + 1)) + minLength;
+  let result = "";
+  for (let j = 0; j < length; j++) {
+    result += getRandomLetter();
+  }
+  return result;
+}
