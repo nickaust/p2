e2a0410a1eb17e5378e6e1febfb2ca0aab0e65e7
Nick Austin <nickaustin27@icloud.com>
2025-04-20 17:02:28 -0700

    First commit

---
+++ p2-random.js
@@ -0,0 +1,18 @@
+/*
+    CIT 281 Project 2
+    Name: Nick Austin
+*/
+
+// Returns a random number between min (inclusive) and max (exclusive)
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  let result = "";
+  
+  for (let i = 0; i < getRandomInteger(5, 26); i++) {
+    result += alphabet[getRandomInteger(1, alphabet.length - 1)];
+  }
+  
+  console.log(result);
